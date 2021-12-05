4d7b2c7eabbe2e40efd77a5c743e36b00e817b9c
Murat Can PARLAR <muratcan.parlar@halkbank.com.tr>
2021-12-05 10:42:17 +0300

    Code Review and deleted waste code

--- MeDirect.Service/GameBoardService.cs
+++ MeDirect.Service/GameBoardService.cs
@@ -19,26 +19,42 @@ namespace MeDirect.Service
             _dbContext = dbContext;
         }
 
+        /// <summary>
+        /// Add new game settings to database
+        /// </summary>
+        /// <param name="gameSetting"></param>
+        /// <returns></returns>
         public async  Task<GameSetting> CreateGameSetting(GameSetting gameSetting)
         {
             await _dbContext.AddAsync(gameSetting);
             await _dbContext.SaveChangesAsync();
             return gameSetting;
         }
-
+        /// <summary>
+        /// Delete Game Setting from database
+        /// </summary>
+        /// <param name="settingId"></param>
+        /// <returns></returns>
         public async Task DeleteGameSetting(Guid settingId)
         {
             var entity = _dbContext.Set<GameSetting>().Where(x => x.Id == settingId).FirstOrDefault();
             _dbContext.Set<GameSetting>().Remove(entity);
             await _dbContext.SaveChangesAsync();
         }
-
+        /// <summary>
+        /// Listing the data of game setting from database
+        /// </summary>
+        /// <returns></returns>
         public async Task<GameSetting> GetGameSettings()
         {
             var gameSetting= await  _dbContext.Set<GameSetting>().FirstOrDefaultAsync();
             return gameSetting;
         }
-
+        /// <summary>
+        /// Update game setting data, which is in the database.
+        /// </summary>
+        /// <param name="gameSetting"></param>
+        /// <returns></returns>
         public async Task UpdateGameSetting(GameSetting gameSetting)
         {
             
@@ -49,7 +65,10 @@ namespace MeDirect.Service
         }
 
 
-
+        /// <summary>
+        /// This Methos calls the Api for Take Board Size. Default Size=5
+        /// </summary>
+        /// <returns></returns>
         async Task<GameSetting> TakeGameBoardSize()
         {
             var result = await _dbContext.Set<GameSetting>().FirstOrDefaultAsync();
@@ -62,6 +81,11 @@ namespace MeDirect.Service
             return result;
         }
 
+        /// <summary>
+        /// This method is connecting to the database for take which ligths are oppening when the creating gameboard.
+        /// </summary>
+        /// <param name="gameSettingId"></param>
+        /// <returns></returns>
         async Task<List<GameLight>> TakeOpenLights(Guid gameSettingId)
         {
 
@@ -69,7 +93,10 @@ namespace MeDirect.Service
             return result;
         }
 
-
+        /// <summary>
+        /// This method is connected to the database for learning which lights are open when the creating game board.
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<BoardRow>> DrawGameBoard()
         {
             var gamesetting = await TakeGameBoardSize();
@@ -104,6 +131,12 @@ namespace MeDirect.Service
             return boardRows;
         }
 
+        /// <summary>
+        /// This Method works after click the board by user. Firstly, clicked cell and  the four adjacent lights are toggled.
+        /// Nextly, is checked the board if all switched turn off.
+        /// </summary>
+        /// <param name="gameBoardClick"></param>
+        /// <returns></returns>
         public GameBoardClick ClickBoard(GameBoardClick  gameBoardClick)
         {
             var result=SwitchLights(gameBoardClick.BoardRows,gameBoardClick.ClickY, gameBoardClick.ClickX);
@@ -112,6 +145,11 @@ namespace MeDirect.Service
             return gameBoardClick;
         }
 
+        /// <summary>
+        /// This method is check the board if all lights are turned off.
+        /// </summary>
+        /// <param name="BoardRows"><list type="BoardRow"></list></param>
+        /// <returns></returns>
         bool IsBoardComplated(List<BoardRow> BoardRows)
         {
             bool isComplated = true;
@@ -119,7 +157,7 @@ namespace MeDirect.Service
             {
                 for (int x = 0; x < BoardRows.Count; x++)
                 {
-                    if (BoardRows[y].Columns[x].col == false)
+                    if (BoardRows[y].Columns[x].col == true)
                     {
                         isComplated = false;
                     }
@@ -128,7 +166,14 @@ namespace MeDirect.Service
             return isComplated;
         }
 
-
+        /// <summary>
+        /// This method is switched the lights, which are adjacent of clicked light, status. 
+        /// if adjacent light is on, it will turned it off.  or if adjacent light is off it will turned it on.
+        /// </summary>
+        /// <param name="BoardRows">The Game Board of Rows</param>
+        /// <param name="ClickY">Clicked Ligth of Y coordinate</param>
+        /// <param name="ClickX">Clicked Ligth of X coordinate</param>
+        /// <returns></returns>
         List<BoardRow> SwitchLights(List<BoardRow> BoardRows ,int ClickY, int ClickX)
         {
             BoardRows[ClickY].Columns[ClickX].col = !BoardRows[ClickY].Columns[ClickX].col;
